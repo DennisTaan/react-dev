f3ccb7af638175ba2d376141440c0d34d3f450bd
DennisTaan <dennistan95@hotmail.com>
2021-06-15 17:43:24 +0200

    Updates

---
+++ precache-manifest.79bbb0537694fe034f4ddc87253b9066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c63cdd96610ee5761b03bf8f89f7cda",
+    "url": "/react-dev/index.html"
+  },
+  {
+    "revision": "92735277ea01923fbc96",
+    "url": "/react-dev/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e6ed5e3ef9efcc3efd4e",
+    "url": "/react-dev/static/css/main.7f65b569.chunk.css"
+  },
+  {
+    "revision": "92735277ea01923fbc96",
+    "url": "/react-dev/static/js/2.347ebb48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-dev/static/js/2.347ebb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ed5e3ef9efcc3efd4e",
+    "url": "/react-dev/static/js/main.dae3dcca.chunk.js"
+  },
+  {
+    "revision": "c573ee79ae051f53c437",
+    "url": "/react-dev/static/js/runtime-main.9b58f853.js"
+  }
+]);
